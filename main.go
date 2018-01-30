f5b250383bc36f1b0066d029b44fd1a32e3a03d1
David Sciamma <dsciamma@sierrawireless.com>
2018-01-30 09:43:15 +0100

    Fix documentation for golint

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// JSON structure expected as Input of the Lambda
+// ReportInput defines the JSON structure expected as Input of the Lambda
 type ReportInput struct {
   Organization string
   GitHubToken string
 }
 
-// General lambda handler
+// HandleRequest is the general lambda handler
 func HandleRequest(ctx context.Context, input ReportInput) (string, error) {
 
   ghreport := report.NewGitHubReport(input.Organization, input.GitHubToken, 7)
